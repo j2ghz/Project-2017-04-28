ee619114b7356e3ddc3b8db205b832c2fc857087
J2ghz <j2.00ghz@gmail.com>
2017-04-24 11:36:08 +0200

    Databse connect

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,16 @@ public class Main {
     private static DB db;
 
     public static void main(String[] args) {
+        System.out.println("Connecting to database");
         try{
-            db=new DB("sa","123456", "1433","localhost");
+            db=new DB("sa","123456", "1432","localhost");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Connecting to database was no successful");
             System.out.println("Exiting");
             return;
         }
+        System.out.println("Connected");
         MainFrame f = new MainFrame();
     }
 }
