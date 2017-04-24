f1057d14aa081d3aba611a2a42d9a85c6b8dc9e4
J2ghz <j2.00ghz@gmail.com>
2017-04-24 12:37:23 +0200

    Login dialog, right db

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
+import Domain.Management.UserManager;
 import Services.Database.DB;
+import UI.Swing.LoginUI;
 import UI.Swing.MainFrame;
 
 public class Main {
-    private static DB db;
 
     public static void main(String[] args) {
         System.out.println("Connecting to database");
+        DB db;
         try {
-            db = new DB("sa", "123456", "1432", "localhost");
+            db = new DB("sa", "123456789", "1434", "localhost");
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println("Connecting to database was no successful");
-            System.out.println("Exiting");
+            System.err.println("Connecting to database was not successful");
+            System.err.println("Exiting");
             return;
         }
         System.out.println("Connected");
 
+        UserManager userManager = new UserManager(db);
+
         MainFrame f = new MainFrame();
+
+        LoginUI loginUI = new LoginUI(f);
+        loginUI.setVisible(true);
+        //if login bad exit
+
     }
 }
---
+++ src/UI/Swing/LoginUI.java
@@ -0,0 +1,10 @@
+package UI.Swing;
+
+
+import javax.swing.*;
+
+public class LoginUI extends JDialog {
+    public LoginUI(JFrame owner) {
+        super(owner);
+    }
+}
