54095305bff30aa5c23894540ec2bbdcc5741bbe
J2ghz <j2.00ghz@gmail.com>
2017-04-24 12:16:10 +0200

    reserveRoom

--- src/Domain/Management/UserManager.java
+++ src/Domain/Management/UserManager.java
@@ -5,20 +5,20 @@ import Services.Database.DB;
 
 
 public class UserManager {
-    private DB db;
     public User currentUser = null;
+    private DB db;
 
     public UserManager(DB db) {
         this.db = db;
     }
 
     public boolean login(String user, String pass) {
-        currentUser=db.getUser(user,pass);
+        currentUser = db.getUser(user, pass);
         return currentUser != null;
     }
 
-    public void logout(){
-        currentUser=null;
+    public void logout() {
+        currentUser = null;
     }
 
 }
--- src/Domain/Reservation/ReservationManager.java
+++ src/Domain/Reservation/ReservationManager.java
@@ -1,17 +1,34 @@
 package Domain.Reservation;
 
 
+import Domain.Model.Room;
+import Domain.Model.RoomReservation;
+import Services.Database.DB;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ReservationManager {
-    public boolean freeRooms(Date date) {
+    private final DB db;
+
+    public ReservationManager(DB db) {
+        this.db = db;
+    }
+
+    public ArrayList<Room> freeRooms(Date date) {
         throw new NotImplementedException();
     }
 
-    public void reserveRoom() {
-        throw new NotImplementedException();
+    public void reserveRoom(Date checkIn, Date checkOut, Room room) {
+        RoomReservation reservation = new RoomReservation();
+        reservation.setCheckIn(checkIn);
+        reservation.setChekcOut(checkOut);
+        reservation.setRoom(room);
+        reservation.setMenus(new ArrayList<>());
+        reservation.setRoomServices(new ArrayList<>());
+
+        db.addRoomReservation(reservation);
     }
 
 }
